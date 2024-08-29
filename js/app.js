4c54d8519dca8ac3acd919751b55ebd84a807c27
serhiikle <kleimovychserhii@gmail.com>
2024-08-29 17:51:13 -0500

    add current year on page

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 const menuBtn = document.querySelector('.menu-btn');
 const headerSection = document.querySelector('.header');
 
+const yearEl = document.querySelector('.year');
+const currentYear = new Date().getFullYear();
+
 menuBtn.addEventListener('click', () => {
 	headerSection.classList.toggle('visible');
 });
+
+yearEl.textContent = currentYear;
